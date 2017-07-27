56bb4258376a9badb8247dc69c0e0bce77f8fa8d
Justin Lau <jlau7248@gmail.com>
2017-07-26 18:06:55 -1000

    finished exercise 1

--- exercises.js
+++ exercises.js
@@ -9,6 +9,14 @@ Now, unleash your coding beast!
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = "Justin";
+var neighborName = "Idk";
+var teacherName = "Miyagi";
+
+console.log(yourName);
+console.log(neighborName);
+console.log(teacherName);
+
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
